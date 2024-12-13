1f84de0176553dfe29ccc0f8acec4c599a5c52d2
Mr-Moosebear <mr.moosebear86@gmail.com>
2024-12-13 07:04:23 -0500

    Bird flop. No Flap

---
+++ Characters/huebird.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	velocity += get_gravity() * delta
+	move_and_slide()
+
+func get_input() -> void:
+	if Input.is_action_just_pressed("flap"):
+		velocity.y = JUMP_VELOCITY
---
+++ Scenes/level_one.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+@onready var background : TextureRect = $Background
+
+# TODO: Update with a new Background every frame
+# Might save us some time on level builds
+func _process(delta: float) -> void:
+	pass
