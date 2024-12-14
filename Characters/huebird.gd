752abc327214721f500b6887933228bc41165bbb
Mr-Moosebear <mr.moosebear86@gmail.com>
2024-12-13 22:00:33 -0500

    Moving bird with background

--- Characters/huebird.gd
+++ Characters/huebird.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func _ready() -> void:
+	$AnimatedSprite2D.play("default")
 
 func _physics_process(delta: float) -> void:
+	get_input()
 	move_and_slide()
 
 func get_input() -> void:
--- Components/FowardComponent.gd
+++ Components/FowardComponent.gd
@@ -1,1 +1,9 @@
+class_name CompFowardMovement
 extends Node
+
+@export var character = CharacterBody2D
+var speed = 300
+
+
+func _physics_process(delta: float) -> void:
+	character.velocity.x = speed
