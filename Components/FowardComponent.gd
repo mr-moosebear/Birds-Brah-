5502d7a8c509c3b6de0dd004c2dce80f17efa017
aitoiaita <aitoiaita@host>
2024-12-22 18:51:31 -0500

    remove fowardmovementcomponent

--- Components/FowardComponent.gd
+++
@@ -1,9 +0,0 @@
-class_name CompFowardMovement
-extends Node
-
-@export var character = CharacterBody2D
-var speed = 300
-
-
-func _physics_process(delta: float) -> void:
-	character.velocity.x = speed
