a8c058fd63cc8424a2a7d6047db3a86664c73026
aitoiaita <aitoiaita@host>
2024-12-22 23:38:25 -0500

    remove unused line

--- Components/ObstacleMoveComponent.gd
+++ Components/ObstacleMoveComponent.gd
@@ -8,4 +8,3 @@ var speed : float = -300.0
 
 func _physics_process(delta: float) -> void:
 	body.position.x += speed * delta
-	var rightmost_pixel = shape.global_position[0] + shape.shape.get_rect().size[0]
