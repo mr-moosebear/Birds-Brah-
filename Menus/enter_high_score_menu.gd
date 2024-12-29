cb7fca3b2a42eb78c504eb412275df322e042cbb
Mr-Moosebear <mr.moosebear86@gmail.com>
2024-12-29 00:06:44 -0500

    Uncommented stuff

--- Menus/enter_high_score_menu.gd
+++ Menus/enter_high_score_menu.gd
@@ -2,13 +2,13 @@ extends Control
 
 var player_name : String = "Player"
 var leader_dict : Dictionary
-var player_score : String = "9500"
-var player_place : String = "2"
+var player_score : String# = "9500"
+var player_place : String# = "2"
 var new_dict : Dictionary
 
 func _ready() -> void:
-	#player_place = str(Global.placement)
-	#player_score = str(Global.high_score)
+	player_place = str(Global.placement)
+	player_score = str(Global.high_score)
 	$ScoreLabel.text = player_score
 	leader_dict = Global.load_leaderboard()
 
@@ -35,8 +35,8 @@ func set_player_info(dict: Dictionary, place: String, player: String, score: Str
 func _on_submit_button_pressed() -> void:
 	new_dict = reset_leaderboard_dictionary(leader_dict, player_place)
 	var modified_dict = set_player_info(new_dict, player_place, player_name, player_score)
-	#Global.save_leaderboard(modified_dict)
-	#get_tree().change_scene_to_file("res://Menus/leader_board_menu.tscn")
+	Global.save_leaderboard(modified_dict)
+	get_tree().change_scene_to_file("res://Menus/leader_board_menu.tscn")
 	print(modified_dict)
 
 func _on_name_input_text_changed(new_text: String) -> void:
