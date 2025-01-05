4cc816f937f5e6f72e69fb3cca33cab0fa9ce8a0
aitoiaita <aitoiaita@host>
2025-01-05 03:15:23 -0500

    Add beginnings of level select menu

---
+++ Menus/level_select.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var high_score = Global.local_leaderboard.get_high_score()
+	if high_score >= 15000:
+		$LevelBackground2/LockedControl.set_visible(false)
+		$LevelBackground2/Level2Button.set_disabled(false)
+	if high_score >= 30000:
+		$LevelBackground3/LockedControl.set_visible(false)
+		$LevelBackground3/Level3Button.set_disabled(false)
+	if high_score >= 50000:
+		$LevelBackground4/LockedControl.set_visible(false)
+		$LevelBackground4/Level4Button.set_disabled(false)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://Menus/main_menu.tscn")
+
+func _on_level_1_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+func _on_level_2_button_pressed():
+	print("2")
+
+func _on_level_3_button_pressed():
+	print("3")
+
+func _on_level_4_button_pressed():
+	print("4")
--- Menus/main_menu.gd
+++ Menus/main_menu.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	get_tree().change_scene_to_file("res://Menus/level_select.tscn")
 
 
 func _on_bird_select_button_pressed() -> void:
