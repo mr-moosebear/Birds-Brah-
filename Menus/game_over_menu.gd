ca55853fc00e666f1ee03ff11123fba9a453a450
aitoiaita <aitoiaita@host>
2025-01-05 05:11:27 -0500

    Refactor high score entry UI to overlay the normal Game Over screen.

--- Menus/game_over_menu.gd
+++ Menus/game_over_menu.gd
@@ -4,13 +4,13 @@ var player_score : int
 
 func _ready() -> void:
 	self.player_score = Global.score
-	$ScoreLabel.text = str(self.player_score)
+	$ScoreLabel.text = Global.format_int_with_commas(self.player_score)
 
 	if self.player_score > Global.local_leaderboard.get_high_score():
-		enter_high_score_menu()
-	else:
-		Global.score = 0
+		$HighScoreMenu/HighScoreLabel.set_text(Global.format_int_with_commas(self.player_score))
+		$HighScoreMenu.set_visible(true)
 	$HighScoreNumberLabel.text = Global.format_int_with_commas(Global.local_leaderboard.get_high_score())
+	Global.score = 0
 
 func enter_high_score_menu() -> void:
 	get_tree().change_scene_to_file("res://Menus/enter_high_score_menu.tscn")
@@ -20,3 +20,10 @@ func _on_bird_select_button_pressed() -> void:
 
 func _on_continue_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+func _on_high_score_submit_button_pressed():
+	var name = $HighScoreMenu/HighScorePlayerNameInput.get_text()
+	var placement_idx = Global.local_leaderboard.register_score(name, self.player_score)
+	Global.save_leaderboard()
+	Global.local_leaderboard_view_idx = placement_idx
+	get_tree().change_scene_to_file("res://Menus/leader_board_menu.tscn")
